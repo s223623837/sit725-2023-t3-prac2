40301f1537ebcd46f314eecacf7d6774cc95a544
bidhan <bidhanbabugupta@gmail.com>
2023-11-17 10:25:07 +1100

    practical to add two numbers using api

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 8080
+app.listen(port, () => {
+    console.log(`listening at port ${port}`)
+})
+app.get('/addTwoNumbers', (req, res) => {
+    res.json({
+        status: true,
+        sum: Number(req.query.number1) + Number(req.query.number2)
+    })
+})
