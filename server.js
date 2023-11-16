f0fe9940ac55b829f2d49d2e7c871a470a5f4008
bidhan <bidhanbabugupta@gmail.com>
2023-11-17 10:48:52 +1100

    added index.html file

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const port = process.env.port || 8080
 app.listen(port, () => {
     console.log(`listening at port ${port}`)
 })
+app.use(express.static(__dirname + '/'));
+
+app.get('/', (req, res)=>{
+    res.render('index.html');
+});
+
 app.get('/addTwoNumbers', (req, res) => {
     res.json({
         status: true,
